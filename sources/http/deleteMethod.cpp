06cf321f2fc1d6dc9713e739d0b7a13c9bd4387b
rdendonc <rachel.dendoncker@gmail.com>
2025-02-03 16:18:54 +0100

    start delete method

--- sources/http/deleteMethod.cpp
+++ sources/http/deleteMethod.cpp
@@ -9,18 +9,43 @@ void HandleRequests::initDeleteInfos(WebServer &webServData)
 	size_t slashPos = this->_fileName.rfind('/');
 	if (slashPos != std::string::npos)
 		this->_fileName = this->_fileName.substr(slashPos + 1);
-	std::cout << std::endl;
-	std::cout << std::endl;
 	this->_filePath = this->_rootDir + this->_fileName;
 	std::cout << "COUCOU 1 !! [" << this->_fileName << "]" << std::endl;
 	std::cout << "COUCOU 2 !! [" << this->_filePath << "]" << std::endl;
 	std::cout << "COUCOU 3 !! [" << this->_rootDir << "]" << std::endl;
 }
 
+static std::string createDeleteResponseHeader(const std::string &statusCode)
+{
+    std::ostringstream headerStream;
+    headerStream << "HTTP/1.1 " << statusCode << " \r\n";
+    headerStream << "Content-Type: text/plain\r\n";
+    headerStream << "Content-Length: 0\r\n";
+    headerStream << "Connection: keep-alive\r\n";
+    headerStream << "\r\n";
+    return headerStream.str();
+}
+
 void HandleRequests::deleteMethod(WebServer &webServData)
 {
-	
 	initDeleteInfos(webServData);
-	std::cout << this->_buffer << std::endl;
-	return ;
+
+	if (access(this->_filePath.c_str(), F_OK) == -1) {
+		this->_response = createDeleteResponseHeader("404 Not Found");
+		return;
+	}
+
+	if (remove(this->_filePath.c_str()) == 0)
+		this->_response = createDeleteResponseHeader("200 OK");
+	else
+		this->_response = createDeleteResponseHeader("403 Forbidden");
+
+	struct epoll_event event;
+	event.events = EPOLLOUT | EPOLLET;
+	event.data.fd = this->_clientFd;
+	if (epoll_ctl(this->_epollFd, EPOLL_CTL_MOD, this->_clientFd, &event) == -1)
+	{
+		perror("EPOLL_CTL MOD ERROR");
+		return;
+	}
 }
