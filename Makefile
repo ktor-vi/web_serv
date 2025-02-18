34e666214bfdf66aec0c329f6b8086b7959f1b57
rdendonc <rachel.dendoncker@gmail.com>
2025-02-18 11:21:50 +0100

    add flag -Werror

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ EXECUTABLE_NAME = web_serv.exe
 
 # Compilateur et options
 COMPILATOR = c++
-FLAGS = -Wall -Wextra -g3 -std=c++98 #-Werror -Ofast -funroll-loops
+FLAGS = -Wall -Wextra -Werror -g3 -std=c++98 # -Ofast -funroll-loops
 
 # Répertoires
 SRC_DIR = sources
--- sources/main.cpp
+++ sources/main.cpp
@@ -2,34 +2,33 @@
 
 void handle_sigint(int sig) {
 	(void)sig;
-    exit(0);
+	exit(0);
 }
 
-int main()
+int main(void)
 {
-  	Config config;
+	Config config;
 
-  if (signal(SIGINT, handle_sigint) == SIG_ERR)
+	if (signal(SIGINT, handle_sigint) == SIG_ERR)
 	{
-        perror("Erreur lors de l'enregistrement du signal SIGINT");
-        exit(1);
-  }
+		perror("Erreur lors de l'enregistrement du signal SIGINT");
+		exit(1);
+	}
 	if (config.parseFile("config.conf"))
 	{
-    	WebServer web_serv(config);
-	try
-	{
-    	web_serv.printServer();
-    	web_serv.verifyServer();
-		ft_webserver(web_serv);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << "Error: " << e.what() << '\n';
-	}
-	
-  } 
-  else
-  	std::cerr << "Erreur lors du parsing du fichier de configuration." << std::endl;
-  return 0;
+		WebServer web_serv(config);
+		try
+		{
+			web_serv.printServer();
+			web_serv.verifyServer();
+			ft_webserver(web_serv);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << "Error: " << e.what() << '\n';
+		}
+	} 
+	else
+		std::cerr << "Erreur lors du parsing du fichier de configuration." << std::endl;
+	return (0);
 }
